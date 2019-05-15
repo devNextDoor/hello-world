c32f2365de0b3681e8fe711b393f0537c78a3019
solitude79 <31189972+solitude79@users.noreply.github.com>
2019-05-15 12:03:23 +0530

    Dijkstra's Single Source Shortest Path

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,16 +1,14 @@
 #include<bits/stdc++.h>
-#define PII pair<int,int>
+#define PII pair<long,int>
+#define LONG_MAX 1000000000
 using namespace std;
-vector<PII> adj[100002];
-bool vis[100002];
-long dist[100002];
+vector<PII> adj[10002];
+bool vis[10002];
+long dist[10002];
 void dijkstra()
 {
-    memset(vis,false,sizeof(vis));
-    memset(dist,200000,sizeof(dist));
     priority_queue<PII,vector<PII>,greater<PII> > Q;
     Q.push(make_pair(0,1));
-    vis[1]=true;
     dist[1]=0;
     while(!Q.empty())
     {
@@ -21,13 +19,11 @@ void dijkstra()
         vis[n]=true;
         for(int i=0;i<adj[n].size();i++)
         {
-            if(!vis[adj[n][i].first)
+            int e=adj[n][i].first; long w=adj[n][i].second;
+            if(!vis[e] && dist[e]>dist[n]+w)
             {
-                if(dist[adj[n][i].first]>dist[n]+adj[n][i].second)
-                {
-                    dist[adj[n][i].first]=(dist[n]+adj[n][i].second);
-                }
-                Q.push(make_pair(adj[n][i].second,adj[n][i].first));
+                    dist[e]=(dist[n]+w);
+                    Q.push(make_pair(dist[e],e));
             }
         }
     }
@@ -36,13 +32,18 @@ int main()
 {
     int n,m,x,y,w;
     cin>>n>>m;
+    for(int i=1; i<=n; i++)
+    {
+        dist[i]=LONG_MAX;
+        vis[i]=0;
+    }
     for(int i=0;i<m;i++)
     {
         cin>>x>>y>>w;
-        adj[x].push_back(make_pair(y,w));
+        adj[x].push_back(make_pair(y,w));       
         adj[y].push_back(make_pair(x,w));
     }
     dijkstra();
-    for(int i=1;i<=n;i++)
-        cout<<dist[i]<<"\n";
+    for(int i=2;i<=n;i++)
+        cout<<dist[i]<<" ";
 }
